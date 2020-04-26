49bcca9705cf6a5aba111c70fa1cc85043f4b8d9
Psophonthamm <Panithan.Sophonthammakhun@agoda.com>
2020-04-26 14:24:53 +0700

    add Balance component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Header } from './components/Header'
+import { Balance } from './components/Balance'
 
 function App() {
   return (
     <div>
-      <Header text='Expense Tracker'/>
+      <Header text='Expense Tracker' />
+      <div className="Container">
+        <Balance />
+      </div>
     </div>
   );
 }
