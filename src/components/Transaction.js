93b04ee6060c0f811bbb5518c038733f49993fa5
Psophonthamm <Panithan.Sophonthammakhun@agoda.com>
2020-04-26 15:56:00 +0700

    impl delete transaction

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import getAmountText from '../utils/getAmountText'
+
+import { GlobalContext } from '../context/GlobalState'
 
 export const Transaction = ({ transaction }) => {
     
-    const { text, amount } = transaction
+    const { deleteTransaction } = useContext(GlobalContext)
 
+    const { text, amount } = transaction
     const getTransactionClassName = (amount) => amount < 0 ? 'minus' : 'plus'
-    
+
     return (
         <li className={getTransactionClassName(amount)}>
             {text}
             <span>{getAmountText(amount)}</span>
-            <button className="delete-btn">x</button>
+            <button className="delete-btn" onClick={() => deleteTransaction(transaction.id)}>x</button>
         </li>
     )
 }
