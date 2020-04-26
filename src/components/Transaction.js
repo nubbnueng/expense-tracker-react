2063036e650d9be5c15d3956085a625b8d1248e3
Psophonthamm <Panithan.Sophonthammakhun@agoda.com>
2020-04-26 15:42:26 +0700

    refactor transaction component

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import getAmountText from '../utils/getAmountText'
 
 export const Transaction = ({ transaction }) => {
+    
     const { text, amount } = transaction
 
     const getTransactionClassName = (amount) => amount < 0 ? 'minus' : 'plus'
-    const getAmountText = (amount) => amount < 0 ? `-$${amount * -1}` : `$${amount}`
-
+    
     return (
         <li className={getTransactionClassName(amount)}>
             {text}
