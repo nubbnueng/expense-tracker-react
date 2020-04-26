263c3eac6690c1e1bf0d62b6456dd8491e921a8f
Psophonthamm <Panithan.Sophonthammakhun@agoda.com>
2020-04-26 21:50:52 +0700

    fix typo

--- src/utils/formatAmount.js
+++ src/utils/formatAmount.js
@@ -2,10 +2,10 @@ const numberWithCommas = (x) => {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 }
 
-const amountWithdDollarSign = (amount) => {
+const amountWithDollarSign = (amount) => {
     return amount < 0 ? `-$${amount * -1}` : `$${amount}`
 }
 
 export default (amount) => {
-    return numberWithCommas(amountWithdDollarSign(amount))
+    return numberWithCommas(amountWithDollarSign(amount))
 }
