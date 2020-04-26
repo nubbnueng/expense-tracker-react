c40eae887cd56348b19b213cf83a2a504674734c
Psophonthamm <Panithan.Sophonthammakhun@agoda.com>
2020-04-26 15:43:14 +0700

    refactor TransactionSummary component

--- src/components/TransactionSummary.js
+++ src/components/TransactionSummary.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import getAmountText from '../utils/getAmountText'
+import { GlobalContext } from '../context/GlobalState'
 
 export const TransactionSummary = () => {
+
+    const { transactions } = useContext(GlobalContext)
+
+    const getTotal = (filter) => transactions
+        .map(transaction => transaction.amount)
+        .filter(amount => filter(amount))
+        .reduce((acc, amount) => acc += amount, 0)
+
+    const totalIncome = getTotal(amount => amount > 0)
+    const totalExpense = getTotal(amount => amount < 0)
+
     return (
         <>
             <div className="inc-exp-container">
                 <div>
                     <h4>Income</h4>
-                    <p className="money plus">+$0.00</p>
+                    <p className="money plus">{getAmountText(totalIncome)}</p>
                 </div>
                 <div>
                     <h4>Expense</h4>
-                    <p className="money minus">-$0.00</p>
+                    <p className="money minus">{getAmountText(totalExpense)}</p>
                 </div>
             </div>
         </>
