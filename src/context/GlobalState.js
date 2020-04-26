ded2845e5c3ecdefcae973676459a88a7009426d
Psophonthamm <Panithan.Sophonthammakhun@agoda.com>
2020-04-26 15:28:48 +0700

    remove unuse

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,4 +1,4 @@
-import React, { createContext, useReducer, Children } from 'react'
+import React, { createContext, useReducer } from 'react'
 import AppReducer from './AppReducer'
 
 //initial state
